cb06c71351fd408597ee267607583c85b9ac1a17
Diego Martinez Giardini <83225359+kida10-tech@users.noreply.github.com>
2022-01-06 12:02:47 -0300

    Update Ejercitacion Tema 1.sql

--- Ejercitacion Tema 1.sql
+++ Ejercitacion Tema 1.sql
@@ -1,3 +1,19 @@
+/*
+Cree una tabla llamada CURSO con los atributos:
+Código de curso (clave primaria, entero no nulo)
+Nombre (varchar no nulo)
+Descripcion (varcha)
+Turno (varchar no nulo)
+Agregue un campo “cupo” de tipo numérico
+Inserte datos en la tabla:
+(101, “Algoritmos”,”Algoritmos y Estructuras de Datos”,”Mañana”,35)
+(102, “Matemática Discreta”,””,”Tarde”,30)
+Intente ingresar un registro con el nombre nulo y verifique que no funciona.
+Intente ingresar un registro con la clave primaria repetida y verifique que no funciona.
+Actualice, para todos los cursos, el cupo en 25.
+Elimine el curso Algoritmos.
+*/
+
 CREATE TABLE curso (
 	codigo INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
   	nombre VARCHAR(50) NOT NULL,
