8531d522e93ecd9d42f7e36210f18d09427bae53
Diego Martinez Giardini <diego.martinez.giardini@gmail.com>
2022-01-06 11:57:58 -0300

    Tema 1 resuelto

---
+++ Ejercitacion Tema 1.sql
@@ -0,0 +1,17 @@
+CREATE TABLE curso (
+	codigo INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+  	nombre VARCHAR(50) NOT NULL,
+  	descripcion VARCHAR(150) NULL,
+  	turno VARCHAR(20) NOT NULL
+);
+
+ALTER TABLE curso ADD cupo INT;
+
+INSERT INTO `curso` VALUES (101,'Algoritmos','Algoritmos y Estructura de datos','Mañana',35);
+INSERT INTO `curso` VALUES (102,'Matematica Discreta','','Tarde',30);
+INSERT INTO `curso` VALUES (104,'Aprendiendo desarrollo de software','Tarde',25);
+INSERT INTO `curso` VALUES (101,'Java para todos','Aprendiendo desarrollo de software','Tarde',25);
+
+UPDATE curso SET cupo = 25;
+
+DELETE FROM curso WHERE nombre = 'Algoritmos';
