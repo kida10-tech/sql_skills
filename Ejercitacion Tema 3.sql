2aeac5dc972391d8097c08e2151abe0604c2951e
Diego Martinez Giardini <83225359+kida10-tech@users.noreply.github.com>
2022-01-06 16:46:56 -0300

    Update Ejercitacion Tema 3.sql

--- Ejercitacion Tema 3.sql
+++ Ejercitacion Tema 3.sql
@@ -1,3 +1,12 @@
+/*
+- Nombre, apellido y fecha de nacimiento de todos los empleados, ordenado por fecha de nacimiento ascendente.
+- Todos los profesores cuyo salario sea mayor o igual a 65000.
+- Todos los profesores que nacieron en la década del 80.
+- 5 registros
+- Todos los profesores cuyo apellido inicie con la letra “P”
+- Los profesores que nacieron en la década del 80 y tienen un salario mayor a 80000
+*/
+
 -- 1
 SELECT nombre, apellido, fecha_nacimiento
 FROM profesor
@@ -5,7 +14,7 @@ ORDER BY fecha_nacimiento ASC;
 
 -- 2
 SELECT * FROM profesor
-WHERE salario >= 45000;
+WHERE salario >= 65000;
 
 -- 3
 SELECT * FROM profesor
@@ -14,7 +23,7 @@ BETWEEN '1980-01-01' AND '1989-12-31';
 
 -- 4
 SELECT * FROM profesor
-LIMIT 2;
+LIMIT 5;
 
 -- 5
 SELECT * FROM profesor
@@ -24,4 +33,4 @@ WHERE apellido LIKE 'P%';
 SELECT * FROM profesor
 WHERE fecha_nacimiento
 BETWEEN '1980-01-01' AND '1989-12-31'
-AND salario > 50000;
+AND salario > 80000;
